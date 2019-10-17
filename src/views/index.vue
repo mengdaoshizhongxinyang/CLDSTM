9ee7851381e9eb71d593ac4193e0174c8b51b060
mengdaoshizhongxinyang <729403918@qq.com>
2019-10-17 09:15:01 +0800

    add snowy back

--- src/views/index.vue
+++ src/views/index.vue
@@ -27,13 +27,17 @@
 import * as device from "@/../public/device.min.js";
 import * as bundle from "@/../public/bundle.js";
 import VueMD from "vue-markdown";
-import * as three from "three";
+import * as THREE from "three";
+console.log(THREE)
 import config from "@/../public/config.js";
-
-
-console.log(config);
+// console.log(()=>import("three.proton.js"))
+import Proton from "./proton.js";
+let proton, emitter;
+let camera, scene, renderer;
+let three = new THREE.Scene();
+console.log(Proton)
 if (config.signboard === true) {
-  var modelUrl = "./haru02/haru02.model.json";
+  let modelUrl = "./haru02/haru02.model.json";
   loadlive2d("live2dcanvas", modelUrl);
 }
 export default {
@@ -43,7 +47,7 @@ export default {
   data() {
     return {
       openKeys: [],
-      content: '',
+      content: "",
       url: "@/../public/static/test.md",
       leftTree: config.file
     };
@@ -56,7 +60,94 @@ export default {
           this.content = res.default;
         });
       });
+    },
+    addScene() {
+      camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        3000
+      );
+      camera.position.z = 500;
+      scene = new THREE.Scene();
+      scene.fog = new THREE.FogExp2(0xffffff, 1, 10000);
+
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      document.body.appendChild(renderer.domElement);
+      window.addEventListener("resize", this.onWindowResize, false);
+    },
+    addProton() {
+      console.log(1)
+      proton = new Proton();
+      
+      emitter = new Proton.Emitter();
+      emitter.rate = new Proton.Rate(
+        new Proton.Span(34, 48),
+        new Proton.Span(0.2, 0.5)
+      );
+      emitter.addInitialize(new Proton.Mass(1));
+      emitter.addInitialize(new Proton.Radius(new Proton.Span(10, 20)));
+
+      var position = new Proton.Position();
+      position.addZone(new Proton.BoxZone(2500, 10, 2500));
+      emitter.addInitialize(position);
+
+      emitter.addInitialize(new Proton.Life(5, 10));
+      emitter.addInitialize(new Proton.Body(this.createSnow()));
+      emitter.addInitialize(
+        new Proton.Velocity(0, new Proton.Vector3D(0, -1, 0), 90)
+      );
+
+      emitter.addBehaviour(new Proton.RandomDrift(10, 1, 10, 0.05));
+      emitter.addBehaviour(new Proton.Rotate("random", "random"));
+      emitter.addBehaviour(new Proton.Gravity(2));
+
+      var sceenZone = new Proton.ScreenZone(camera, renderer, 20, "234");
+      emitter.addBehaviour(new Proton.CrossZone(sceenZone, "dead"));
+
+      emitter.p.x = 0;
+      emitter.p.y = 800;
+      emitter.emit();
+
+      proton.addEmitter(emitter);
+      proton.addRender(new Proton.SpriteRender(scene));
+
+      //Proton.Debug.drawZone(proton,scene,new Proton.BoxZone(800, 10, 800));
+    },
+    createSnow() {
+      var map = new THREE.TextureLoader().load("./img/snow.png");
+      var material = new THREE.SpriteMaterial({
+        map: map,
+        transparent: true,
+        opacity: 0.5,
+        color: 0xffffff
+      });
+      return new THREE.Sprite(material);
+    },
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.render();
+    },
+    render() {
+        proton.update();
+        renderer.render(scene, camera);
+    },
+    onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    },
+    initBackGround(){
+      this.addScene();
+      this.addProton();
+      this.animate();
     }
+  },
+  mounted(){
+    this.initBackGround();
   }
 };
 </script>
