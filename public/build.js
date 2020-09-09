a293271f848a8fe19f4d18240a9d4ac8c72bdb6f
mengdaoshizhongxinyang <729403918@qq.com>
2020-09-09 17:19:13 +0800

    chore:add compress function

--- public/build.js
+++ public/build.js
@@ -51,3 +51,107 @@ articleList += "}\n"
 articleList += "//article end//"
 config = config.replace(/.*\/\/article start\/\/[\n|\r|\r\n](.*[\n|\r|\r\n])*.*\/\/article end\/\//, articleList)//\/\/article start\/\/\n.*\n\/\/article end\/\/
 fs.writeFileSync('./config.js', config)
+
+
+function Compress(strNormalString) {
+        let strCompressedString = "";
+    
+        let ht = new Array();
+        for(i = 0; i < 128; i++) {
+            ht[i] = i;
+        }
+    
+        let used = 128;
+        let intLeftOver = 0;
+        let intOutputCode = 0;
+        let pcode = 0;
+        let ccode = 0;
+        let k = 0;
+    
+        for(let i=0; i<strNormalString.length; i++) {
+            ccode = strNormalString.charCodeAt(i);
+            k = (pcode << 8) | ccode;
+            if(ht[k] != null) {
+                pcode = ht[k];
+            } else {
+                intLeftOver += 12;
+                intOutputCode <<= 12;
+                intOutputCode |= pcode;
+                pcode = ccode;
+                if(intLeftOver >= 16) {
+                    strCompressedString += String.fromCharCode( intOutputCode >> ( intLeftOver - 16 ) );
+                    intOutputCode &= (Math.pow(2, (intLeftOver - 16)) - 1);
+                    intLeftOver -= 16;
+                }
+                if(used < 4096) {
+                    used ++;
+                    ht[k] = used - 1;
+                }
+            }
+        }
+    
+        if(pcode != 0) {
+            intLeftOver += 12;
+            intOutputCode <<= 12;
+            intOutputCode |= pcode;
+        }
+    
+        if(intLeftOver >= 16) {
+            strCompressedString += String.fromCharCode( intOutputCode >> ( intLeftOver - 16 ) );
+            intOutputCode &= (Math.pow(2,(intLeftOver - 16)) - 1);
+            intLeftOver -= 16;
+        }
+    
+        if( intLeftOver > 0) {
+            intOutputCode <<= (16 - intLeftOver);
+            strCompressedString += String.fromCharCode( intOutputCode );
+        }
+    
+        return strCompressedString;
+    }
+    function Decompress(strCompressedString) {
+        let strNormalString = "";
+        let ht = new Array();
+    
+        for(i = 0; i < 128; i++) {
+            ht[i] = String.fromCharCode(i);
+        }
+    
+        let used = 128;
+        let intLeftOver = 0;
+        let intOutputCode = 0;
+        let ccode = 0;
+        let pcode = 0;
+        let key = 0;
+    
+        for(let i=0; i<strCompressedString.length; i++) {
+            intLeftOver += 16;
+            intOutputCode <<= 16;
+            intOutputCode |= strCompressedString.charCodeAt(i);
+    
+            while(1) {
+                if(intLeftOver >= 12) {
+                    ccode = intOutputCode >> (intLeftOver - 12);
+                    if( typeof( key = ht[ccode] ) != "undefined" ) {
+                         strNormalString += key;
+                        if(used > 128) {
+                            ht[ht.length] = ht[pcode] + key.substr(0, 1);
+                        }
+                         pcode = ccode;
+                    } else {
+                        key = ht[pcode] + ht[pcode].substr(0, 1);
+                        strNormalString += key;
+                        ht[ht.length] = ht[pcode] + key.substr(0, 1);
+                        pcode = ht.length - 1;
+                    }
+    
+                    used ++;
+                    intLeftOver -= 12;
+                    intOutputCode &= (Math.pow(2,intLeftOver) - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return strNormalString;
+    }
