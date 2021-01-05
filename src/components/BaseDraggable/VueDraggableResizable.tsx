e5b6347a54a8204877b9ce0d22926a249349a0ea
mengdaoshizhongxinyang <729403918@qq.com>
2021-01-05 12:34:32 +0800

    chore:update base draggable

--- src/components/BaseDraggable/VueDraggableResizable.tsx
+++ src/components/BaseDraggable/VueDraggableResizable.tsx
@@ -1,37 +1,37 @@
 import { matchesSelectorToParentElements, addEvent, removeEvent } from './utils';
-import { defineComponent, PropType, reactive,getCurrentInstance } from "vue";
-interface props{
-  className:string
-  classNameDraggable:string
-  classNameResizable:string
-  classNameDragging:string
-  classNameResizing:String
-  classNameActive:String
-  classNameHandle:String
-  disableUserSelect:boolean
-  enableNativeDrag:boolean
-  preventDeactivation:boolean
-  active:boolean
-  draggable:boolean
-  resizable:boolean
-  lockAspectRatio:boolean
-  w:number
-  h:number
-  minWidth:number
-  minHeight:number
-  maxWidth:number
-  maxHeight:number
-  x:number
-  y:number
-  z:number|string
-  handles:Array<string>
-  dragHandle:string | null
-  dragCancel:string | null
-  axis:'x' | 'y' | 'both'
-  grid:Array<number>
-  parent:boolean
-  onDragStart:Function
-  onResizeStart:Function
+import { defineComponent, PropType, reactive, getCurrentInstance, ref, computed } from "vue";
+interface props {
+  className: string
+  classNameDraggable: string
+  classNameResizable: string
+  classNameDragging: string
+  classNameResizing: String
+  classNameActive: String
+  classNameHandle: String
+  disableUserSelect: boolean
+  enableNativeDrag: boolean
+  preventDeactivation: boolean
+  active: boolean
+  draggable: boolean
+  resizable: boolean
+  lockAspectRatio: boolean
+  w: number
+  h: number
+  minWidth: number
+  minHeight: number
+  maxWidth: number
+  maxHeight: number
+  x: number
+  y: number
+  z: number | string
+  handles: Array<string>
+  dragHandle: string | null
+  dragCancel: string | null
+  axis: 'x' | 'y' | 'both'
+  grid: [number,number]
+  parent: boolean
+  onDragStart: Function
+  onResizeStart: Function
 }
 const events = {
   mouse: {
@@ -59,7 +59,7 @@ const userSelectAuto = {
   WebkitUserSelect: 'auto',
   MsUserSelect: 'auto'
 }
-const data=reactive({
+const data = reactive({
   rawWidth: 0,
   rawHeight: 0,
   rawLeft: 0,
@@ -87,10 +87,34 @@ const data=reactive({
   enabled: false,
   resizing: false,
   dragging: false,
-  zIndex: 0 as number | string
+  zIndex: 0 as number | string,
+  mouseClickPosition: {} as {
+    mouseX?: number,
+    mouseY?: number,
+    left?: number,
+    right?: number,
+    top?: number,
+    bottom?: number,
+    x?: number,
+    y?: number,
+    w?: number,
+    h?: number
+  },
+  bounds: {} as {
+    minLeft: number | null,
+    maxLeft: number | null,
+    minRight: number | null,
+    maxRight: number | null,
+    minTop: number | null,
+    maxTop: number | null,
+    minBottom: number | null,
+    maxBottom: number | null
+  }
 })
+let eventsFor = events.mouse
+let deselect: (e: MouseEvent) => void
 export default defineComponent({
-  name:'vue-draggable-resizable',
+  name: 'vue-draggable-resizable',
   props: {
     className: {
       type: String,
@@ -151,32 +175,32 @@ export default defineComponent({
     w: {
       type: Number,
       default: 200,
-      validator: (val:number) => val > 0
+      validator: (val: number) => val > 0
     },
     h: {
       type: Number,
       default: 200,
-      validator: (val:number) => val > 0
+      validator: (val: number) => val > 0
     },
     minWidth: {
       type: Number,
       default: 0,
-      validator: (val:number) => val >= 0
+      validator: (val: number) => val >= 0
     },
     minHeight: {
       type: Number,
       default: 0,
-      validator: (val:number) => val >= 0
+      validator: (val: number) => val >= 0
     },
     maxWidth: {
       type: Number,
       default: null,
-      validator: (val:number) => val >= 0
+      validator: (val: number) => val >= 0
     },
     maxHeight: {
       type: Number,
       default: null,
-      validator: (val:number) => val >= 0
+      validator: (val: number) => val >= 0
     },
     x: {
       type: Number,
@@ -191,12 +215,12 @@ export default defineComponent({
     z: {
       type: [String, Number],
       default: 'auto',
-      validator: (val:string|number) => (typeof val === 'string' ? val === 'auto' : val >= 0)
+      validator: (val: string | number) => (typeof val === 'string' ? val === 'auto' : val >= 0)
     },
     handles: {
       type: Array as PropType<Array<string>>,
       default: () => ['tl', 'tm', 'tr', 'mr', 'br', 'bm', 'bl', 'ml'],
-      validator: (val:Array<string>) => {
+      validator: (val: Array<string>) => {
         const s = new Set(['tl', 'tm', 'tr', 'mr', 'br', 'bm', 'bl', 'ml'])
 
         return new Set(val.filter(h => s.has(h))).size === val.length
@@ -213,10 +237,10 @@ export default defineComponent({
     axis: {
       type: String as PropType<props['axis']>,
       default: 'both',
-      validator: (val:string) => ['x', 'y', 'both'].includes(val)
+      validator: (val: string) => ['x', 'y', 'both'].includes(val)
     },
     grid: {
-      type: Array as PropType<Array<number>>,
+      type:  Array as unknown as PropType<props['grid']>,
       default: () => [1, 1]
     },
     parent: {
@@ -232,19 +256,98 @@ export default defineComponent({
       default: null
     }
   },
-  setup(props:props){
-    data.rawWidth=props.w
-    data.rawHeight=props.h
-    data.rawLeft=props.x
-    data.rawTop=props.y
-    data.left=props.x
-    data.top=props.y
-    data.aspectFactor=props.w/props.h
-    data.minW=props.minWidth
-    data.minH=props.minHeight
-    data.maxW=props.maxWidth
-    data.maxH=props.maxHeight
-    data.enabled=props.active
-    data.zIndex=props.z
+  setup(props: props, { emit }) {
+    data.rawWidth = props.w
+    data.rawHeight = props.h
+    data.rawLeft = props.x
+    data.rawTop = props.y
+    data.left = props.x
+    data.top = props.y
+    data.aspectFactor = props.w / props.h
+    data.minW = props.minWidth
+    data.minH = props.minHeight
+    data.maxW = props.maxWidth
+    data.maxH = props.maxHeight
+    data.enabled = props.active
+    data.zIndex = props.z
+    const width=computed(()=>{
+      return data.parentWidth!-data.left-data.right!
+    })
+    const el = ref<HTMLDivElement>().value!
+    const resetBoundsAndMouseState = () => {
+      data.mouseClickPosition = { mouseX: 0, mouseY: 0, x: 0, y: 0, w: 0, h: 0 }
+
+      data.bounds = {
+        minLeft: null,
+        maxLeft: null,
+        minRight: null,
+        maxRight: null,
+        minTop: null,
+        maxTop: null,
+        minBottom: null,
+        maxBottom: null
+      }
+    }
+    const getParentSize=()=>{
+      if (props.parent) {
+        const style = window.getComputedStyle(el.parentNode! as Element, null)
+
+        return [
+          parseInt(style.getPropertyValue('width'), 10),
+          parseInt(style.getPropertyValue('height'), 10)
+        ]
+      }
+
+      return [null, null]
+    }
+    const handleMove=(e:TouchEvent)=>{
+      const handle = data.handle
+      const mouseClickPosition = data.mouseClickPosition
+
+      const tmpDeltaX = mouseClickPosition.mouseX! - e.touches[0].pageX 
+      const tmpDeltaY = mouseClickPosition.mouseY! - e.touches[0].pageY
+
+      const [deltaX, deltaY] = this.snapToGrid(this.grid, tmpDeltaX, tmpDeltaY)
+
+      if (!deltaX && !deltaY) return
+
+      if (handle.includes('b')) {
+        this.rawBottom = mouseClickPosition.bottom + deltaY
+      } else if (handle.includes('t')) {
+        this.rawTop = mouseClickPosition.top - deltaY
+      }
+
+      if (handle.includes('r')) {
+        this.rawRight = mouseClickPosition.right + deltaX
+      } else if (handle.includes('l')) {
+        this.rawLeft = mouseClickPosition.left - deltaX
+      }
+
+      emit('resizing', data.left, data.top, data.width, data.height)
+    }
+    deselect = (e) => {
+      const target = e.target as HTMLBaseElement
+
+      const regex = new RegExp(props.className + '-([trmbl]{2})', '')
+
+      if (!el.contains(target) && !regex.test(target.className)) {
+        if (data.enabled && !props.preventDeactivation) {
+          data.enabled = false
+
+          emit('deactivated')
+          emit('update:active', false)
+        }
+
+        removeEvent(document.documentElement, eventsFor.move, handleMove)
+      }
+
+      resetBoundsAndMouseState()
+    }
+    const snapToGrid=(grid:[number,number], pendingX:number, pendingY:number)=>{
+      const x = Math.round(pendingX / grid[0]) * grid[0]
+      const y = Math.round(pendingY / grid[1]) * grid[1]
+
+      return [x, y]
+    }
   }
 })
